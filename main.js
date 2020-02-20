45b6f6248d06ef623feafef5a3fa485404e907c7
Zach <pro.bizach@gmail.com>
2020-02-20 16:16:22 +0100

    Popin created

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ dropdown.onclick = e => {
 navbarTogler.onclick = e => {
   navbar.classList.toggle("show");
 };
+
+// Plugin For Modal
+$(function() {
+  $.fn.modal = function(state) {
+    console.log($(this));
+    if (state === "show") {
+      $(this).show();
+    } else if (state === "hide") {
+      $(this).hide();
+    }
+  };
+
+  $("button").click(function() {
+    if ($(this).attr("data-target")) {
+      const target = $(this).attr("data-target");
+      $(target).modal("show");
+    } else if ($(this).attr("data-dismiss")) {
+      const dismiss = $(this).attr("data-dismiss");
+      $(dismiss).modal("hide");
+    }
+  });
+});
