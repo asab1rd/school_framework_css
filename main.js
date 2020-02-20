280055d6e9e740b13b443001ac5f0b95671782db
Zach <pro.bizach@gmail.com>
2020-02-20 14:17:51 +0100

    Responsive nav

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // $(function() {
 //   $(document).tooltip();
 // });
+const navbar = document.getElementById("navbarSupportedContent");
+const navbarTogler = document.getElementById("navbarToggler");
+const dropdown = document.getElementById("navbarDropdown");
+const dropdownMenu = document.getElementsByClassName("dropdown-menu")[0];
+dropdown.onclick = e => {
+  dropdownMenu.classList.toggle("show");
+};
+navbarTogler.onclick = e => {
+  navbar.classList.toggle("show");
+};
